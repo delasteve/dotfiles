830ecae32ac953f8451ebf50ceba39c757362c84
Stephen Cavaliere <stephencavaliere@gmail.com>
2015-01-28 16:30:21 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 magenta='\033[35m'
 yellow='\033[33m'
@@ -6,36 +6,36 @@ green='\033[32m'
 clear='\033[0m'
 stepCounter=1
 
-function outputSection() {
-  echo "${magenta}======= ${1} =======${clear}"
+outputSection() {
+  echo -e "${magenta}======= ${1} =======${clear}"
 }
 
-function outputStep() {
-  echo "${green}${stepCounter}) ${yellow}${1}${clear}"
+outputStep() {
+  echo -e "${green}${stepCounter}) ${yellow}${1}${clear}"
   stepCounter=$(( ${stepCounter} + 1 ))
 }
 
-function updateLocalRepository() {
+updateLocalRepository() {
   pushd ${HOME}/.dotfiles &>/dev/null
   git pull origin master &>/dev/null
   popd &>/dev/null
 }
 
-function checkForVundle() {
+checkForVundle() {
   if [ ! -d "${HOME}/.vim/bundle/vundle.vim" ]; then
-    echo "Vundle not found. Installing...${clear}"
+    echo -e "Vundle not found. Installing...${clear}"
     git clone https://github.com/gmarik/vundle.vim.git ~/.vim/bundle/vundle.vim &>/dev/null
   else
-    echo "Vundle found. Skipping...${clear}"
+    echo -e "Vundle found. Skipping...${clear}"
   fi
 }
 
-function checkForUltiSnips() {
+checkForUltiSnips() {
   if [ ! -d "${HOME}/.vim/UltiSnips" ]; then
-    echo "UltiSnips symlink not found. Linking...${clear}"
+    echo -e "UltiSnips symlink not found. Linking...${clear}"
     ln -s ~/.dotfiles/vim/UltiSnips ~/.vim
   else
-    echo "UltiSnips symlink found. Skipping...${clear}"
+    echo -e "UltiSnips symlink found. Skipping...${clear}"
   fi
 }
 
